ad8d89fbcea224ed6b2c317449ddaada4e1c9578
a-brascher <anbrascher@hotmail.com>
2015-04-10 15:22:28 -0300

    generate Possible Guesses List
    Added function for generation of a list of possible guesses. Minor
    changes on generateGuess function

--- src/ai/ComputerAI.java
+++ src/ai/ComputerAI.java
@@ -1,6 +1,8 @@
 package ai;
 
 import utils.Utils;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.lang.*;
@@ -11,7 +13,23 @@ public class ComputerAI extends HumanAI {
 		hand = Utils.randomHand(round);
 	}
 	
-	private int generateGuess(int chopsticks, List possibilities, List guesses){
+	private List getPossibilities(int chopsticks, int othersChopsticks, List guesses){
+		LinkedList<Integer> possibilities = new LinkedList<Integer>();
+		for(int i=0; i<=othersChopsticks; i++){
+			int tmp = chopsticks+i;
+			boolean contains = false;
+			int j = 0;
+			while(!contains && j<guesses.size()){
+				if(tmp==((Integer) guesses.get(j)).intValue()) contains = true;
+				j++;
+			}
+			if(!contains) possibilities.add(new Integer(tmp));
+		}
+		return possibilities;
+	}
+	
+	public void generateGuess(int chopsticks, int othersChopsticks, List guesses){
+		List possibilities = getPossibilities(chopsticks, othersChopsticks, guesses);
 		double standardDeviation = 0;
 		double mean = 0;
 		
@@ -25,12 +43,12 @@ public class ComputerAI extends HumanAI {
 		//mean calculated!
 		
 		for(int i=0; i<guesses.size();i++)
-			standardDeviation += Math.pow( (((int) guesses.get(i)) - mean)*2, 2.0);
+			standardDeviation += Math.pow( (  ((Integer) guesses.get(i)).intValue() - mean)*2, 2.0);
 		
 		for(int i=0; i<possibilities.size();i++)
-			standardDeviation += Math.pow( ((int) possibilities.get(i)) - mean, 2.0);
+			standardDeviation += Math.pow( ((Integer) possibilities.get(i)).intValue() - mean, 2.0);
 
-		standardDeviation = Math.pow( standardDeviation/(guesses.size()*2 + possibilities.size() - 1.5), 0.5); 
+		standardDeviation = Math.pow(standardDeviation/(guesses.size()*2 + possibilities.size() - 1.5), 0.5); 
 			//-1.5 for greater precision on estimation
 		//standard deviation calculated!
 		
@@ -38,16 +56,16 @@ public class ComputerAI extends HumanAI {
 		
 		double tempGuess = (rand.nextGaussian()*standardDeviation) + mean;
 
-		int guess = 0;
-		double closestDiff = Math.abs(((int) possibilities.get(0)) - tempGuess);
+		int guess = ((Integer) possibilities.get(0)).intValue();
+		double closestDiff = Math.abs( ((Integer) possibilities.get(0)).intValue() - tempGuess);
 		for (int i=1; i<possibilities.size(); i++) {
-			double tmp = Math.abs(((int) possibilities.get(i)) - tempGuess);
+			double tmp = Math.abs( ((Integer) possibilities.get(i)).intValue() - tempGuess);
 			if(tmp<closestDiff){
 				guess = i;
 				closestDiff = tmp;
 			}
 		}
 		
-		return guess; //temporary
+		setGuess(guess);
 	}
 }
